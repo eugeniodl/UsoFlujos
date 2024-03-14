f4ca1fd679c7a6c6ba70370e7ee3eae726e65241
LABORATORIO 3 <LABORATORIO 3@DESKTOP-NVKFFL7>
2024-03-14 07:29:40 -0600

    Agregar archivos de proyecto.

---
+++ PruebaFileStream/Program.cs
@@ -0,0 +1,8 @@
+﻿using(Stream fs = new FileStream("clientes.txt",
+    FileMode.Open, FileAccess.Read))
+{
+    // Obtenemos el número de bytes
+    long cantidad = fs.Length;
+    Console.WriteLine( $"El archivo tiene una " +
+        $"longitud de {cantidad} bytes");
+}
